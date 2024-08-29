b25e2fc73b758ca212857966c6177f104feeb7f0
Naimul Islam <nayemhasan1314@gmail.com>
2024-08-29 21:10:26 +0600

    simple try

---
+++ create-react/customreact.js
@@ -0,0 +1,25 @@
+function customRender(reactElement, props) {
+  /*
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  domElement.setAttribute("href", reactElement.props.href);
+  domElement.setAttribute("target", reactElement.props.target);
+  props.appendChild(domElement);
+  */
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  for (const props in reactElement.props) {
+    domElement.setAttribute(props, reactElement.props[props]);
+  }
+  props.appendChild(domElement);
+}
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://www.google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+const mainContainer = document.getElementById("root");
+customRender(reactElement, mainContainer);
